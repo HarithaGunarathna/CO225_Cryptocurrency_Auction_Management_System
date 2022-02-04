ed13c40a016de091d39ead869bb1f2f97df9c5da
IshtaJayakody <73831741+IshtaJayakody@users.noreply.github.com>
2022-02-05 04:44:33 +0530

    get an array into list view

--- user_interface/app/src/main/java/com/example/user_interface/BidMarketActivity.java
+++ user_interface/app/src/main/java/com/example/user_interface/BidMarketActivity.java
@@ -3,12 +3,25 @@ package com.example.user_interface;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 public class BidMarketActivity extends AppCompatActivity {
+
+    String[] currencies = {"Win XP" ,"Ishta" , "Thumula" , "ganidu" , "Chathura"} ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bid_market);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.activty_listview ,currencies) ;
+
+        ListView listView = (ListView)findViewById(R.id.window_list);
+        listView.setAdapter(adapter);
     }
+
+
 }
